c3c9a66814a935be097f54a17c53db9baa1957f2
G.P.Kampouris <georgeokamps@gmail.com>
2023-09-03 13:07:42 +0300

    first Rest Api App

---
+++ src/main/java/gr/aueb/cf/jaxstarter4/StarterApplication.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.jaxstarter4;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class StarterApplication extends Application {
+
+}
---
+++ src/main/java/gr/aueb/cf/jaxstarter4/dto/TeacherUpdateDTO.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.jaxstarter4.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class TeacherUpdateDTO {
+
+    @NotNull(message = "Please fill the id")
+    private long id;
+
+    @Size(min = 6, max = 6, message = "Ssn must be 6 digit long")
+    private String ssn;
+
+    @NotBlank(message = "please fill the firstname")
+    private String firstname;
+
+    @NotBlank(message = "please fill the lastname")
+    private String lastname;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
